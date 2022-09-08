97b0ecd0b9c5b0f0b90501cf444d50ab52c2ae52
ingeniumed <ingeniumed@users.noreply.github.com>
2022-09-08 16:52:26 +1000

    Move the main file outside the src folder for easier running

--- ingest-tokens.js
+++ ingest-tokens.js
@@ -5,8 +5,8 @@ const fs = require( 'fs' ).promises;
 const path = require( 'path' );
 const cp = require( 'child_process' );
 const chalk = require( 'chalk' );
-const styleDictionary = require( './style-dictionary/main' );
-const utility = require( './utility' );
+const styleDictionary = require( './src/style-dictionary/main' );
+const utility = require( './src/utility' );
 
 const transformerFilePath = 'src/build/transformed-tokens.json';
 const generatedThemeFileName = 'theme.generated.json';
